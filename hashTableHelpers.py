b605f85313f5e02f09f666ab80642b62f580bdd5
bzargesg <bzargesg@gmail.com>
2019-07-30 09:42:14 -0600

    build simple array and hashing function for helper class hashtable

---
+++ hashTableHelpers.py
@@ -0,0 +1,35 @@
+import ctypes
+import math
+
+
+class LimitedArray:
+    storage = []
+    limit = 0
+
+    def __init__(self, limit):
+        self.storage
+
+    def get(self, index):
+        self.checkLimit(index)
+        return self.storage[index]
+
+    def checkLimit(self, index):
+        if(not isinstance(index, int) or self.limit <= index):
+            raise IndexError
+
+    def set(self, index, value):
+        self.checkLimit(index)
+        self.storage[index] = value
+
+    def each(self, callback):
+        for item in self.storage:
+            callback(item, self.storage)
+
+
+def getIndexBelowMaxForKey(str, max):
+    hashval = 0
+    for char in str:
+        hashval = ctypes.c_int(hashval << 5 ^ 0).value + hash + ord(char)
+        hashval = hashval & hashval
+        hashval = math.fabs(hashval)
+    return hashval % max
